64a3be2680dcc6b99b37e5cb8c3d23be8bf6762e
lt <mypi.ip.adr@gmail.com>
2017-08-22 01:41:06 +0900

    おほ＾～

---
+++ jyoshi.py
@@ -0,0 +1,24 @@
+# 若い助詞
+import sys
+
+class Jyoshi(object):
+    def __init__(self, name, state):
+        self.name = name
+        self.state = state
+
+    def comingOut(self):
+        self.state = "ネカマ"
+
+def kaizuka_bokki(jyoshi):
+	if jyoshi.state == "処女":
+	    return 100
+	elif jyoshi.state == "非処女":
+	    return 80
+	else:
+		sys.exit("警告： 正体表したね")
+
+
+jyoshi = Jyoshi("野獣先輩", "処女")
+print(jyoshi.name + "は" + str(kaizuka_bokki(jyoshi)) + "点")
+jyoshi.comingOut()
+print(jyoshi.name + "は" + str(kaizuka_bokki(jyoshi)) + "点")
